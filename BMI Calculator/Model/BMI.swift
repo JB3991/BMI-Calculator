04a044c55a075ff795b7d337a414cbfc5a18878a
Jon Burnett <74561172+JB3991@users.noreply.github.com>
2021-02-16 19:13:17 +0000

    Finished project

---
+++ BMI Calculator/Model/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Jonathan Burnett on 16/02/2021.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
